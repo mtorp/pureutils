1f4d68048f6622ae564156ce48485963313d34b7
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2018-08-24 17:09:40 -0700

    splitPromise

--- src/logic.ts
+++ src/logic.ts
@@ -1101,3 +1101,14 @@ export function nextToPromise<T>(obs: rx.Observable<T>): Promise<T> {
     return new Promise<T>((resolve, reject) => obs.subscribe(resolve, reject));
 }
 
+/**Crea una nueva promesa y devuelve por separado la promesa y las funciones que resuelven y rechazan a la promesa */
+export function splitPromise<T>(): { promise: Promise<T>, resolve: (value?: T | PromiseLike<T> | undefined) => void, reject: (reason?: any) => void } {
+    let resolve = null as any;
+    let reject = null as any;
+    const promise = new Promise<T>((promResolve, promReject) => {
+        resolve = promResolve;
+        reject = promReject;
+    });
+
+    return { promise, resolve, reject };
+}
