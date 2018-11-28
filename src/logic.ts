92e845c8a9e0a7d3ef18bc21c27861f876b66c28
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2018-11-28 15:23:32 -0700

    shuffle

--- src/logic.ts
+++ src/logic.ts
@@ -1183,3 +1183,20 @@ export function outOfRange(value: number, range: FloatRange): RangeCheckResult |
         return "max";
     return null;
 }
+
+/**Barajea un arreglo */
+export function shuffle<T>(arr: T[]): T[] {
+    function shuffleInPlace(a) {
+        let j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+    }
+    const ret = [... arr];
+    shuffleInPlace(ret);
+
+    return ret;
+}
